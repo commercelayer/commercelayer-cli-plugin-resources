bbaecdda49a341f89b2763b2d511c6c002f22f46
Pierluigi Viti <pierluigi@commercelayer.io>
2023-08-22 15:42:23 +0200

    feat(core): update resources

--- src/util/resources/available.ts
+++ src/util/resources/available.ts
@@ -83,6 +83,7 @@ const RESOURCES = [
 	{ name: 'promotion', api: 'promotions', model: 'Promotion' },
 	{ name: 'recurring_order_copy', api: 'recurring_order_copies', model: 'RecurringOrderCopy' },
 	{ name: 'refund', api: 'refunds', model: 'Refund' },
+	{ name: 'reserved_stock', api: 'reserved_stocks', model: 'ReservedStock' },
 	{ name: 'return_line_item', api: 'return_line_items', model: 'ReturnLineItem' },
 	{ name: 'return', api: 'returns', model: 'Return' },
 	{ name: 'satispay_gateway', api: 'satispay_gateways', model: 'SatispayGateway' },
